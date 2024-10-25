250d117c9c6a4ccfc32fb1f4ed051a07aee74db9
Pancreass <mragankmuni000@gmail.com>
2024-10-25 18:42:27 +0530

    basic ui done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,11 +7,11 @@ function Navbar() {
     <div className='h-16 w-full'>
       <div className='flex justify-between items-center py-2 px-6 border-b '>
         <div className="flex items-center space-x-2">
-          <Image className='size-14 rounded-full bg-gray-600' alt='yo' src=''/>
+          <Image className='size-12 rounded-full bg-gray-600' alt='yo' src=''/>
           <p className='font-bold text-3xl px-3'>ETERNA</p>
         </div>
         <div className='flex'>
-          <Image className='rounded-full size-14 bg-pink-600' alt='ok' src=''/>
+          <Image className='rounded-full size-12 bg-pink-600' alt='ok' src=''/>
         </div>
       </div>
     </div>
