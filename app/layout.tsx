78bc29b72a61304f35d892712b20ae215b3c1411
roshni <roshni.23545@sscbs.du.ac.in>
2024-10-26 01:40:59 +0530

    added authentication and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google";
 
 import Navbar from "../components/Navbar"
+import { ClerkProvider } from "@clerk/nextjs";
+const inter = Inter({ subsets: ["latin"] });
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,13 +18,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <body className={inter.className}>
         <Navbar/>
-        {children}
+          <main>
+            {children}
+          </main>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
